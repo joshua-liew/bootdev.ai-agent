bb18aec70b0d169cda47c2435b6e5cc5e16a9cc8
Joshua Liew <liqshann@gmail.com>
2025-10-17 14:00:03 +0900

    refactor: prompt -> user_prompt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from dotenv import load_dotenv
-from google import genai
+from google import genai, types
 
 
 def main():
@@ -10,8 +10,7 @@ def main():
         print("Usage: main.py [prompt]")
         print(f"Error: expected {NUM_OF_ARGS} arguments; received {len(sys.argv)} - exiting with code 1")
         exit(1)
-
-    prompt = str(sys.argv[1])
+    user_prompt = str(sys.argv[1])
 
     print("Hello from ai-agent!")
     load_dotenv()
@@ -19,7 +18,7 @@ def main():
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=prompt
+        contents=user_prompt
     )
     print(response.text)
 
