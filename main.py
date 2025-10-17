e898562591d5379b3d43b7064bb9ea2a762fe9ae
Joshua Liew <liqshann@gmail.com>
2025-10-17 14:06:15 +0900

    feat: add msg roles to store context

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import sys
 from dotenv import load_dotenv
-from google import genai, types
+from google import genai
+from google.genai import types
 
 
 def main():
@@ -16,9 +17,18 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    # Store roles
+    messages = [
+        types.Content(
+            role="user",
+            parts=[types.Part(text=user_prompt)]
+        ),
+    ]
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=user_prompt
+        contents=messages
     )
     print(response.text)
 
