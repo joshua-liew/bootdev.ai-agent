3e70d841dc7edce7f5add7f790c39355915a8fbc
Joshua Liew <liqshann@gmail.com>
2025-10-18 11:07:00 +0900

    refactor: generate_content func

--- main.py
+++ main.py
@@ -25,19 +25,30 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    # Store roles
-    #messages = [
-    #    types.Content(
-    #        role="user",
-    #        parts=[types.Part(text=user_prompt)]
-    #    ),
-    #]
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
 
-    #response = client.models.generate_content(
-    #    model='gemini-2.0-flash-001',
-    #    contents=messages
-    #)
-    #print(response.text)
+    # Store roles
+    messages = [
+        types.Content(
+            role="user",
+            parts=[types.Part(text=user_prompt)]
+        ),
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages,
+    )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
