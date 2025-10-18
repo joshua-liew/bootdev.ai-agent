973259454d9a931bdbee279608dd10df66f6c317
Joshua Liew <liqshann@gmail.com>
2025-10-18 15:50:27 +0900

    refactor: remove redundant get_abs_path

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,8 +2,8 @@ import os
 
 
 def get_files_info(working_directory, directory="."):
-    wdir_abs_path = get_abs_path(working_directory)
-    dir_abs_path = get_abs_path(os.path.join(working_directory, directory))
+    wdir_abs_path = os.path.abspath(working_directory)
+    dir_abs_path = os.path.abspath(os.path.join(working_directory, directory))
     # CHECK: validate within working directory boundary
     if not dir_abs_path.startswith(wdir_abs_path):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
@@ -16,15 +16,6 @@ def get_files_info(working_directory, directory="."):
 
     contents = get_dir_contents(dir_abs_path)
     return contents
-
-
-def get_abs_path(path):
-    try:
-        abs_path = os.path.abspath(path)
-    except Exception as err:
-        return f'Error: {err=}'
-    else:
-        return abs_path
 
 
 def get_dir_contents(dir_path):
