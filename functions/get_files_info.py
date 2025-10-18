aa83aff23b2af8a32157ceb119b9efb16c279f6f
Joshua Liew <liqshann@gmail.com>
2025-10-18 13:24:27 +0900

    refactor: get_abs_path w/ error catching

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,22 +1,30 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    work_dir_path_abs = os.path.abspath(working_directory)
-    user_dir_path_full = os.path.join(work_dir_path_abs, directory)
-    user_dir_path_abs = os.path.abspath(user_dir_path_full)
+    wdir_abs_path = get_abs_path(working_directory)
+    dir_abs_path = get_abs_path(directory)
     # CHECK: validate within working directory boundary
-    if not work_dir_path_abs in user_dir_path_abs:
+    if not wdir_abs_path in dir_abs_path:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
     # CHECK: directory is a file
-    if os.path.isfile(user_dir_path_abs):
+    if os.path.isfile(dir_abs_path):
         return f'Error: "{directory}" is not a directory'
 
     # CHECK: if directory or file actually exists
 
-    user_dir_content_list = os.listdir(user_dir_path_abs)
+    user_dir_content_list = os.listdir(dir_abs_path)
     user_dir_content_info = list(map(
         lambda item: f'- {item}: file_size={os.path.getsize(item)} bytes, is_dir={os.path.isdir(item)}',
         user_dir_content_list
     ))
     return "\n".join(user_dir_content_info)
+
+
+def get_abs_path(path):
+    try:
+        abs_path = os.path.abspath(path)
+    except Exception as err:
+        return f'Error: {err=}, {type(err)=}'
+    else:
+        return abs_path
