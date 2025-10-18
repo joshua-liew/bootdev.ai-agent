a34d2737982aa5124a9111b380255e621f6ac8d9
Joshua Liew <liqshann@gmail.com>
2025-10-18 11:47:16 +0900

    feat: validate path if dir or file

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,9 +4,14 @@ def get_files_info(working_directory, directory="."):
     work_dir_path_abs = os.path.abspath(working_directory)
     user_dir_path_full = os.path.join(work_dir_path_abs, directory)
     user_dir_path_abs = os.path.abspath(user_dir_path_full)
+    # CHECK: validate within working directory boundary
     if not work_dir_path_abs in user_dir_path_abs:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    print("work_dir_path_abs:", work_dir_path_abs)
-    print("user_dir_path_full:", user_dir_path_full)
-    print("user_dir_path_abs:", user_dir_path_abs)
-    return f'Success'
+
+    # CHECK: directory is a file
+    if os.path.isfile(user_dir_path_abs):
+        return f'Error: "{directory}" is not a directory'
+
+    # CHECK: if directory or file actually exists
+
+    return f'Success: path is {user_dir_path_abs}'
