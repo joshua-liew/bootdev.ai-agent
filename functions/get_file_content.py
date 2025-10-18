04bd71462be8a7f11c746eabe5538f222ee49c41
Joshua Liew <liqshann@gmail.com>
2025-10-18 17:53:59 +0900

    refactor: clean up, remove hard-coded vals

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,7 +5,6 @@ from .config import *
 def get_file_content(working_directory, file_path):
     wdir_abs_path = os.path.abspath(working_directory)
     file_abs_path = os.path.abspath(os.path.join(working_directory, file_path))
-    # CHECK: validate within working directory boundary
     if not file_abs_path.startswith(wdir_abs_path):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.isfile(file_abs_path):
@@ -15,7 +14,9 @@ def get_file_content(working_directory, file_path):
         with open(file_abs_path, "r") as f:
             file_content = f.read(FILE_CONTENT_MAX_SIZE)
             if len(file_content) == FILE_CONTENT_MAX_SIZE:
-                file_content += f'[...File "{file_path}" truncated at 10000 characters]'
+                file_content += (
+                    f'[...File "{file_path}" truncated at {FILE_CONTENT_MAX_SIZE} characters]'
+                )
             return file_content
     except Exception as err:
-        return f'Error: {err=}'
+        return f'Error reading file "{filepath}": {err=}'
