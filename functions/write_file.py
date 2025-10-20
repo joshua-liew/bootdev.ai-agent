9b870e270a5f4606caf76c42a3a2760fbf2bd5fa
Joshua Liew <liqshann@gmail.com>
2025-10-20 10:15:04 +0900

    (ch2, l4) solution files

--- functions/write_file.py
+++ functions/write_file.py
@@ -8,12 +8,17 @@ def write_file(working_directory, file_path, content):
     if not file_abs_path.startswith(wdir_abs_path):
         return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
 
-    file_rel_path = os.path.join(working_directory, file_path)
-    if not os.path.isfile(file_abs_path):
+    # CHECK: validate if path exists - if path exists open and write
+    # if path does not exist, create directory, open and write
+    if not os.path.exists(file_abs_path):
         try:
-            open(file_rel_path, 'x')
+            os.makedirs(os.path.dirname(file_abs_path), exist_ok=True)
         except Exception as err:
-            return f'Error: cannot create file "{file_rel_path}"; {err=}'
+            return f'Error: could not create directory {err=}'
+
+    file_rel_path = os.path.join(working_directory, file_path)
+    if os.path.exists(file_abs_path) and os.path.isdir(file_abs_path):
+        return f'Error: "{file_rel_path} is a directory; not a file'
 
     try:
         with open(file_abs_path, 'w') as f:
