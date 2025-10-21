316dced6dcad0cbb3ddb52a194525aeb1b917910
Joshua Liew <liqshann@gmail.com>
2025-10-21 10:06:12 +0900

    refactor: return values & function_calls

--- main.py
+++ main.py
@@ -55,10 +55,12 @@ def generate_content(client, messages, verbose):
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
-    if response.function_calls:
-        for func in response.function_calls:
-            print(f"Calling function: {func.name}({func.args})")
-    print(response.text)
+
+    if not response.function_calls:
+        return response.text
+
+    for func_call in response.function_calls:
+        print(f"Calling function: {func_call.name}({func_call.args})")
 
 
 if __name__ == "__main__":
