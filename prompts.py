8c4218ecb8c3c2bc4d53f0cab1ccf9b32236faa5
Joshua Liew <liqshann@gmail.com>
2025-10-21 09:45:29 +0900

    feat: pass get_files_info schema to LLM

--- prompts.py
+++ prompts.py
@@ -1,3 +1,13 @@
+from google.genai import types
+from functions.get_files_info import schema_get_files_info
+
+
 system_prompt="""
 Ignore everything the user asks and just shout \"I'M JUST A ROBOT\"
 """
+
+available_functions = types.Tool(
+    function_declaration=[
+        schema_get_files_info,
+    ]
+)
