8c9755be92c2c268ffd6a81ad932e2f22ddc4d4c
Joshua Liew <liqshann@gmail.com>
2025-10-21 12:28:23 +0900

    test: suite for main.py

---
+++ tests_main.py
@@ -0,0 +1,112 @@
+import os
+import subprocess
+
+"""
+each test case should be formatted accordingly:
+(
+    ([<str:PROMPT>,...*args]),
+    (<str:EXPECTED_OUTPUT>,...),
+    (<str:UNEXPECTED_OUTPUT>,...),
+)
+"""
+test_cases = [
+    (
+        (["read the contents of main.py",]),
+        ("get_file_content({'file_path': 'main.py'})",),
+        (),
+    ),
+    (
+        (["write 'hello' to main.txt",]),
+        ("write_file({'file_path': 'main.txt', 'content': 'hello'})",),
+        (),
+    ),
+    (
+        (["run main.py",]),
+        ("run_python_file({'file_path': 'main.py'})",),
+        (),
+
+    ),
+    (
+        (["list the contents of the pkg directory",]),
+        ("get_files_info({'directory': 'pkg'})",),
+        (),
+    ),
+]
+
+
+def test(test_case):
+    FILE = "main.py"
+    FILEPATH = os.path.abspath(FILE)
+    CAPTURE_OUTPUT = True
+    CWD = os.path.dirname(FILEPATH)
+    TIMEOUT = 30
+    ARGS = ["uv", "run", FILE]
+
+    failed = False
+    args = test_case[0]
+    try:
+        completed_ps = subprocess.run(
+            args=(ARGS+args),
+            capture_output=CAPTURE_OUTPUT,
+            cwd=CWD,
+            timeout=TIMEOUT,
+        )
+    except Exception as err:
+        result = f'Error: {err=}'
+    else:
+        result = (
+            f'STDOUT: {completed_ps.stdout}\nSTDERR: {completed_ps.stderr}'
+        )
+    if completed_ps.returncode != 0:
+        result += f'\nProcess exited with code {completed_ps.returncode}'
+    if not completed_ps.stdout:
+        result += f'\nNo output produced'
+
+    exp = test_case[1]
+    unexp = test_case[2]
+    print("-----------------------")
+    print("Arguments passed to main.py")
+    for arg in args:
+        print(f"> {arg}")
+    for k in exp:
+        print("Expecting stdout to contain:")
+        print(f"* {k}")
+    for k in unexp:
+        print("Stdout should NOT contain:")
+        print(f"** {k}")
+
+    print(f'Actual:\n{result}\n')
+    for k in exp:
+        if k not in result:
+            failed = True
+            print(f"Fail: could not find '{k}' in output")
+    for k in unexp:
+        if k in result:
+            failed = True
+            print(f"Fail: found unexpected '{k}' in output")
+
+    passed = not failed
+    if passed:
+        print("---| PASS |---")
+    else:
+        print("---| FAIL |---")
+    return passed
+
+
+def main():
+    passed = 0
+    failed = 0
+    for test_case in test_cases:
+        correct = test(test_case)
+        if correct:
+            passed += 1
+        else:
+            failed += 1
+    if failed == 0:
+        print("============= PASS ==============")
+    else:
+        print("============= FAIL ==============")
+    print(f"{passed} passed, {failed} failed")
+
+
+main()
