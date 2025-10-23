d1cd10fdef35f577ae94fcfe794c30f23890f164
Joshua Liew <liqshann@gmail.com>
2025-10-23 17:03:26 +0900

    feat: sys prompt with 'DO NOT MAKE MISTAKES'

--- prompts.py
+++ prompts.py
@@ -9,4 +9,8 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+When asked to perform actions, you must perform them in relation to the context of the working directory and current files/directories. When fixing bugs, find out which file the fix should be made in, look at the existing code and perform fixes within the existing code. When writing new code, look at the existing code and judge if the new code can be written in existing files before creating new files.
+
+DO NOT MAKE MISTAKES.
 """
